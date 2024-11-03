a0a04ad1e12082a1d7f446d6d8c7a1a8cd6bde26
Vo Huy Chu <s4075654@rmit.edu.vn>
2024-11-04 00:58:58 +0700

    Completed problem 1 of week 5

---
+++ W5/problem_1/src/main/java/a_maven_project/your_application/MyHelloHighLevelController.java
@@ -0,0 +1,28 @@
+package a_maven_project.your_application;
+
+import java.util.Map;
+
+import com.massisframework.massis.model.agents.HighLevelController;
+import com.massisframework.massis.model.agents.LowLevelAgent;
+
+public class MyHelloHighLevelController extends HighLevelController {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MyHelloHighLevelController(LowLevelAgent agent, Map<String, String> metadata, String resourcesFolder) {
+		super(agent, metadata, resourcesFolder);
+		this.agent.setHighLevelData(this);
+	}
+
+	@Override
+	public void stop() {
+		/*
+		 * Clean resources, threads...etc
+		 */
+	}
+
+	@Override
+	public void step() {
+		System.out.println("Hey! I am an agent!");
+	}
+}
